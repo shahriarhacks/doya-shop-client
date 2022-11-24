28d716a5c7361a7ee9c956ec56c8191c0141f01c
Shahriar Ahmed <shahriar7ahmed@gmail.com>
2022-11-24 09:54:50 +0600

    Login/Register Page Setup

---
+++ src/pages/login-register/Register.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  const [signUpError, setSignUpError] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const handleRegister = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className="h-[800px] flex justify-center items-center">
+      <div className="w-96 p-7">
+        <h2 className="text-xl text-center">Register</h2>
+        <div>
+          {signUpError && <p className="text-red-600">{signUpError}</p>}
+        </div>
+        <form onSubmit={handleSubmit(handleRegister)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              {...register("name", {
+                required: "Name is Required",
+              })}
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.name && (
+              <p className="text-red-500">{errors.name.message}</p>
+            )}
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              {...register("email", {
+                required: "Email is required",
+              })}
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be 6 characters long",
+                },
+                pattern: {
+                  value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
+                  message:
+                    "Password must have uppercase, number and special characters",
+                },
+              })}
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Account Type</span>
+            </label>
+            <select
+              {...register("role")}
+              className="select select-ghost w-full max-w-xs"
+            >
+              <option value="user">User</option>
+              <option value="seller">Seller</option>
+            </select>
+          </div>
+          <input
+            className="btn btn-accent w-full mt-4 hover:rounded-full"
+            value="Register"
+            type="submit"
+          />
+        </form>
+        <p>
+          Already have an account{" "}
+          <Link className="text-secondary" to="/login">
+            Login here
+          </Link>
+        </p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline w-full hover:rounded-full">
+          CONTINUE WITH GOOGLE
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- src/routes/router.js
+++ src/routes/router.js
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
 import About from "../pages/about/About";
 import Home from "../pages/home/Home";
+import Login from "../pages/login-register/Login";
+import Register from "../pages/login-register/Register";
 import Blog from "../pages/others/Blog";
 import Catagories from "../pages/shared/Catagories";
 import Contact from "../pages/shared/Contact";
@@ -30,6 +32,14 @@ const router = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
       },
     ],
   },
