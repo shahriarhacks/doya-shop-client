fb8f3a7c1e725d1369b08dc576e49d6d36f6ec6e
Shahriar Ahmed <shahriar7ahmed@gmail.com>
2022-11-26 10:08:49 +0600

    Delete User Done

--- src/pages/dashboard/Admin/AdminTable.js
+++ src/pages/dashboard/Admin/AdminTable.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { toast } from "react-toastify";
 import useHeaderGET from "../../../hooks/useHeaderGET";
+import ConfirmationModal from "../../shared/ConfirmationModal";
 import AdminTableBody from "./AdminTableBody";
 
 const AdminTable = ({ users, refetch }) => {
   const header = useHeaderGET();
 
+  const [deleteUser, setDeleteUser] = useState(null);
+
+  const closeModal = () => {
+    setDeleteUser(null);
+  };
+
   const handleDelete = (user) => {
     console.log(user);
+    fetch(`${process.env.REACT_APP_API_URl}/users/admin/${user?._id}`, {
+      method: "DELETE",
+      headers: header,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          refetch();
+          toast.success(
+            `Delete confirm ${deleteUser?.role} ${deleteUser?.name}`
+          );
+        }
+      });
   };
 
   const handleUpdate = (id) => {
@@ -45,10 +65,19 @@ const AdminTable = ({ users, refetch }) => {
               index={idx}
               handleUpdate={handleUpdate}
               handleDelete={handleDelete}
+              setDeleteUser={setDeleteUser}
             />
           ))}
         </tbody>
       </table>
+      <ConfirmationModal
+        deleteAction={handleDelete}
+        closeModal={closeModal}
+        modalData={deleteUser}
+        title={`Are you sure you want to delete ${deleteUser?.role} ${deleteUser?.name}`}
+        message={`If you delete ${deleteUser?.role} ${deleteUser?.name} it can't be undone previous position`}
+        actionButton="Delete"
+      />
     </>
   );
 };
--- src/pages/dashboard/Admin/AdminTableBody.js
+++ src/pages/dashboard/Admin/AdminTableBody.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { HiBadgeCheck } from "react-icons/hi";
 
-const AdminTableBody = ({ user, index, handleDelete, handleUpdate }) => {
+const AdminTableBody = ({ user, index, handleUpdate, setDeleteUser }) => {
+  // eslint-disable-next-line no-unused-vars
+
   return (
     <>
       <tr>
@@ -26,12 +28,13 @@ const AdminTableBody = ({ user, index, handleDelete, handleUpdate }) => {
           )}
         </td>
         <td>
-          <button
-            onClick={() => handleDelete(user)}
-            className="btn btn-xs bg-red-400 border-0 text-black hover:rounded-full hover:text-white"
+          <label
+            onClick={() => setDeleteUser(user)}
+            htmlFor="confirmation-modal"
+            className="btn btn-xs btn-error"
           >
             Delete
-          </button>
+          </label>
         </td>
         <td>{user?.role}</td>
       </tr>
